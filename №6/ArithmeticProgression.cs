d3720e12aa74b555386e072b4d26842596821bf2
tyhgjdf <mr.larinus2@mail.ru>
2021-11-27 10:31:05 +0300

    Добавьте файлы проекта.

---
+++ №6/ArithmeticProgression.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6
+{
+    public class ArithmeticProgression : IIndexableSeries
+    {
+        public double this[int index] => _start + (index - 1) * _step;
+
+        private double _start;
+        private int _step;
+        private double _current;
+        public ArithmeticProgression(double start, int step)
+        {
+            _start = start;
+            _step = step;
+            _current = start;
+        }
+
+        public double GetCurrent()//получение текущего элемента прогресси
+        {
+            return _current;
+        }
+
+        public bool MoveNext()//следующий элемент прогрессии
+        {
+            _current = _current + _step;
+            return true;
+        }
+
+        public void Reset()//Сбросить до начального значениялох
+        {
+            _current = _start;
+        }
+    }
+}
---
+++ №6/GeometricProgression.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6
+{
+    public class GeometricProgression : IIndexableSeries
+    {
+        public double this[int index] => _start*Math.Pow(_step, index-1);
+
+        private double _start;//Начальное значение
+        private int _step;//Шаг
+        private double _current;//Текущее значение
+
+        public GeometricProgression( double start, int step)
+        {
+            _start = start;
+            _step = step;
+            _current=_start;
+        }
+        public double GetCurrent()
+        {
+            return _current;
+        }
+
+        public bool MoveNext()
+        {
+            _current = _current * _step;
+            return true;
+        }
+
+        public void Reset()
+        {
+            _current = _start;
+        }
+    }
+}
